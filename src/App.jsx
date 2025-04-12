bcd1b429fdb29781e36775054b659f347e5105ff
satyamshorrf <satyamshorrf@gmail.com>
2025-04-12 22:20:27 +0530

    Navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,43 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React from "react"
+import { Routes, Route } from "react-router-dom"
 
 // Pages
-import HomePage from "./pages/HomePage.jsx";
-import ArtistPage from "./pages/ArtistPage.jsx";
-import AlbumPage from "./pages/AlbumPage.jsx";
-import SongPage from "./pages/SongPage.jsx";
-import SearchPage from "./pages/SearchPage.jsx";
-import PlaylistPage from "./pages/PlaylistPage.jsx";
-import NotFoundPage from "./pages/NotFoundPage.jsx";
+import HomePage from "./pages/HomePage"
+import ArtistPage from "./pages/ArtistPage"
+import AlbumPage from "./pages/AlbumPage"
+import SongPage from "./pages/SongPage"
+import SearchPage from "./pages/SearchPage"
+import PlaylistPage from "./pages/PlaylistPage"
+import NotFoundPage from "./pages/NotFoundPage"
 
 // Components
-import Navbar from "./components/Navbar.jsx";
-import Footer from "./components/Footer.jsx";
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import MusicPlayer from "./components/MusicPlayer"
 
 function App() {
   return (
     <div className="relative z-10 pt-20 min-h-screen bg-black text-white">
       <Navbar />
 
-      <div className="pb-24">
+      <div className="pb-32">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/artists/:artistId" element={<ArtistPage />} />
-          <Route path="/albums/:albumId" element={<AlbumPage />} />
-          <Route path="/songs/:songId" element={<SongPage />} />
+          <Route path="/artists" element={<ArtistPage />} />
+          <Route path="/album" element={<AlbumPage />} />
+          <Route path="/songs" element={<SongPage />} />
           <Route path="/search" element={<SearchPage />} />
-          <Route path="/playlists/:playlistId" element={<PlaylistPage />} />
+          <Route path="/playlist" element={<PlaylistPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </div>
 
       <div className="fixed bottom-0 left-0 w-full z-50">
+        <MusicPlayer />
         <Footer />
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Search, User } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header className="fixed top-0 left-0 w-full bg-black text-white z-50 shadow-md">
+      <div className="container mx-auto px-4 py-4 flex items-center justify-between">
+        {/* Logo & Title */}
+        <Link to="/" className="flex items-center gap-2 text-cyan-300 text-2xl font-bold">
+          <img src="/logo.svg" alt="TuneNest Logo" className="w-8 h-8" />
+          TuneNest
+        </Link>
 
-export default Navbar
+        {/* Navigation Links */}
+        <nav className="hidden md:flex gap-6 items-center">
+          <Link to="/" className="hover:text-cyan-400 font-small transition">
+            HOME
+          </Link>
+          <Link to="/songs" className="hover:text-cyan-400 font-small transition">
+            SONGS
+          </Link>
+          <Link to="/albums" className="hover:text-cyan-400 font-small transition">
+            ALBUMS
+          </Link>
+          <Link to="/artists" className="hover:text-cyan-400 font-small transition">
+            ARTISTS
+          </Link>
+        </nav>
+
+        {/* Icons */}
+        <div className="flex gap-4 items-center">
+          <button className="p-2 rounded-full hover:bg-gray-800 transition">
+            <Search className="w-5 h-5" />
+          </button>
+          <button className="p-2 rounded-full hover:bg-gray-800 transition">
+            <User className="w-5 h-5" />
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
